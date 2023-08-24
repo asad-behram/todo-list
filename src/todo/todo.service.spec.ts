2cb888f1952907a9377e35d925fbb3066f547ad1
asad <asadbehram17@gmail.com>
2023-08-24 16:39:23 +0500

    added specs for services

--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,33 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TodoService } from 'src/todo/todo.service';
-import { Todo } from 'src/schema/todo.schema';
+import { TodoService } from './todo.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Todo } from '../schema/todo.schema';
+import { CreateTaskDto } from '../dto/create-task.dto';
 
 describe('TodoService', () => {
   let service: TodoService;
+  let model: Model<Todo>;
 
+  const mockTodoService = {
+    findById: jest.fn(),
+    find: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+    create: jest.fn(),
+  };
+  const task = {
+    _id: '64dcbf2258f6d775b3e63ef8',
+    task: 'new task',
+    description: 'new description',
+    completed: true,
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [
+        TodoService,
+        {
+          provide: getModelToken(Todo.name),
+          useValue: mockTodoService,
+        },
+      ],
     }).compile();
 
     service = module.get<TodoService>(TodoService);
+    model = module.get<Model<Todo>>(getModelToken(Todo.name));
+  });
+
+  it('should create a new task', async () => {
+    const createTask: CreateTaskDto = {
+      task: 'new task',
+      description: 'new description',
+      completed: true,
+    };
+    const createdTask: CreateTaskDto = {
+      task: 'new task',
+      description: 'new description',
+      completed: true,
+    };
+    model.create = jest.fn().mockResolvedValue(createTask);
+    const result = await service.createTask(createTask);
+    expect(result).toEqual(createdTask);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it('should create a task', () => {
-    const task: Todo = {
-      task: 'new task',
-      description: 'new description',
-      completed: true,
-    };
-    expect(service.createTask(task)).toEqual(task);
+  it('should find a task', async () => {
+    jest.spyOn(model, 'findById').mockResolvedValue(task);
+    const result = await service.findTask(task._id);
+    expect(result).toEqual(task);
   });
 
-  it('should find all tasks', () => {
-    const tasks: Todo[] = [
+  it('should find all tasks', async () => {
+    const tasks = [
       {
         task: 'new task 1',
         description: 'new description 1',
@@ -39,45 +76,44 @@ describe('TodoService', () => {
         completed: false,
       },
     ];
-    tasks.forEach((task) => service.createTask(task));
-    expect(service.getAllTasks()).toEqual(tasks);
+    jest.spyOn(model, 'find').mockResolvedValue(tasks);
+    const result = await service.getAllTasks();
+    expect(result).toEqual(tasks);
   });
 
-  it('should find task by ID', async () => {
-    const task: Todo = {
-      task: 'new task',
-      description: 'new description',
-      completed: false,
-    };
-    const createdTask = await service.createTask(task);
-    expect(service.findTask(createdTask._id)).toEqual(task);
-  });
-
-  it('should update a task', async () => {
-    const task: Todo = {
-      task: 'new task',
-      description: 'new description',
-      completed: false,
-    };
-    const updatedTask: Todo = {
+  it('should update the task', async () => {
+    const updatedTask = {
       task: 'updated task',
       description: 'updated description',
-      completed: true,
+      completed: false,
     };
-    const createdTask = await service.createTask(task);
-    expect(service.updateTask(createdTask._id, updatedTask)).toEqual(
+    jest.spyOn(model, 'findByIdAndUpdate').mockResolvedValue(updatedTask);
+    const result = await service.updateTask(task._id, updatedTask);
+
+    expect(model.findByIdAndUpdate).toHaveBeenCalledWith(
+      task._id,
       updatedTask,
+      { new: true },
     );
+    expect(result).toEqual(updatedTask);
   });
 
-  it('should delete a task', async () => {
-    const task: Todo = {
-      task: 'updated task',
-      description: 'updated description',
-      completed: true,
-    };
-    const createdTask = await service.createTask(task);
-    expect(service.deleteTask(createdTask._id)).toEqual(task);
-    expect(service.findTask(createdTask._id)).toBeNull();
+  describe('delete task', () => {
+    it('should delet the task', async () => {
+      const deletedTask = {
+        task: 'updated task',
+        description: 'updated description',
+        completed: false,
+      };
+      jest.spyOn(model, 'findByIdAndDelete').mockResolvedValue(deletedTask);
+      const result = await service.deleteTask(task._id);
+      expect(result).toEqual(deletedTask);
+    });
+
+    it('should return error', async () => {
+      const error = new Error('error');
+      jest.spyOn(model, 'findByIdAndDelete').mockRejectedValue(error);
+      await expect(service.deleteTask(task._id)).rejects.toThrow(error);
+    });
   });
 });
